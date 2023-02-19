3fde68f17e75d75a1c17f8ce46dd75554b149bd1
oleg_livitskyi <olivitskyi@gmail.com>
2023-02-19 20:30:21 +0100

    Local Camunda setup on Java 8

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,16 +1,18 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
 
 @Deployment(resources = "Tweet.bpmn")
 @ExtendWith(ProcessEngineCoverageExtension.class)
@@ -20,13 +22,22 @@ public class ProcessJUnitTest {
     public void testHappyPath() {
         // Create a HashMap to put in variables for the process instance
         Map<String, Object> variables = new HashMap<String, Object>();
-        variables.put("approved", true);
         variables.put("content", "Exercise 4 test - Test name");
         // Start process with Java API and variables
-        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
-        // Make assertions on the process instance
-        //execute(job());
-        assertThat(processInstance).isEnded();
+        ProcessInstance processInstance = BpmnAwareTests.runtimeService().startProcessInstanceByKey("TweetProcess", variables);
+        assertThat(processInstance).isWaitingAt("ReviewTweetTask");
+        assertThat(task()).hasCandidateGroup("management").isNotAssigned();
+        List<Task> taskList = taskService()
+                .createTaskQuery()
+                .taskCandidateGroup("management")
+                .processInstanceId(processInstance.getId())
+                .list();
+        org.assertj.core.api.Assertions.assertThat(taskList).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(taskList).hasSize(1);
+        Task task = taskList.get(0);
+        Map<String, Object> approvedMap = new HashMap<String, Object>();
+        approvedMap.put("approved", true);
+        taskService().complete(task.getId(), approvedMap);
     }
 
 }
