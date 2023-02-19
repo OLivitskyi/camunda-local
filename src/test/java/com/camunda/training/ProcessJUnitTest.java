6bc4c5a9eec81bcc98e830584e3d5e8331a36c4b
oleg_livitskyi <olivitskyi@gmail.com>
2023-02-19 14:52:57 +0100

    Local Camunda setup on Java 8

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,29 +1,32 @@
 package com.camunda.training;
+
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
+
 @Deployment(resources = "Tweet.bpmn")
-@ExtendWith(ProcessEngineExtension.class)
+@ExtendWith(ProcessEngineCoverageExtension.class)
 public class ProcessJUnitTest {
-  
- @Test
-  public void testHappyPath() {
-    // Create a HashMap to put in variables for the process instance
-    Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("approved", true);
-    // Start process with Java API and variables
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
-    // Make assertions on the process instance
-     //execute(job());
-    assertThat(processInstance).isActive();
-  }
+
+    @Test
+    public void testHappyPath() {
+        // Create a HashMap to put in variables for the process instance
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("approved", true);
+        variables.put("content", "Exercise 4 test - Test name");
+        // Start process with Java API and variables
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+        // Make assertions on the process instance
+        //execute(job());
+        assertThat(processInstance).isEnded();
+    }
 
 }
